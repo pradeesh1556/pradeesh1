83a21b3d8f7fcce321a9ad169690946d574d42e4
pradeesh1556 <pradeeshraj318@gmail.com>
2025-10-04 12:01:00 +0530

    Pradeesh1

---
+++ javascript.js
@@ -0,0 +1,31 @@
+var factor= "Tamil"
+
+function favourite()
+{
+    console.log ("Favourite actor is:"+factor)
+}
+
+favourite()
+
+
+
+function add(a,b)
+{
+    console.log(a+b)
+}
+
+add(30,50)
+
+
+
+var rain = true
+
+if (rain)
+{
+    console.log("Wear a Rain coat")
+}
+
+else{
+
+    console.log("Enjoy the sunbath")
+}
